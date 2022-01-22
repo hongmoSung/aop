e5a04ca2fb8126bc08fb7c2c42af61fa288fae17
hongmo sung <tesung30@naver.com>
2022-01-22 15:58:56 +0900

    스프링 AOP 구현1 - 시작

---
+++ src/main/java/hello/aop/order/aop/AspectV1.java
@@ -0,0 +1,19 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+
+
+    @Around("execution(* hello.aop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // joinPoint 시그니처
+        return joinPoint.proceed();
+    }
+}
