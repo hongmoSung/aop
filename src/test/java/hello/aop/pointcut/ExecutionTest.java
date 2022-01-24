b93e349fbd2e3b8e159a3e8d8247a87ac6203159
sunghongmo <hm.sung@softwareinlife.com>
2022-01-24 21:08:23 +0900

    예제 만들기

---
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,27 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ExecutionTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void printMethod() {
+        log.info("helloMethod = {}", helloMethod);
+    }
+
+}
